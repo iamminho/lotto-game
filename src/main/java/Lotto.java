66259be809be687a42fa4008ecf1ed62330f3dbf
iamminho <minho100227@kakao.com>
2022-11-28 14:37:48 +0900

    feat: 로또 맞는 개수와 보너스 번화 맞춘 유무를 반환하는 기능 구현

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -6,4 +8,17 @@ public class Lotto {
     public Lotto(List<Integer> lotto) {
         this.lotto = lotto;
     }
+
+    public int checkLottoNumberCount(List<Integer> lottoNumber) {
+        return (int) lotto.stream()
+                .filter(number -> lottoNumber.contains(number))
+                .count();
+    }
+
+    public boolean checkHasBonusNumber(int bonusNumber) {
+        return lotto.stream()
+                .anyMatch(number -> number == bonusNumber);
+    }
 }
+
+
